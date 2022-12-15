dc3595d41c5639cac7d7a56f4a3ebc150f43b249
sp9324 <116882883+sp9324@users.noreply.github.com>
2022-12-15 14:08:34 +0530

    Update assignmentq5.py

--- assignmentq5.py
+++ assignmentq5.py
@@ -2,10 +2,10 @@ import math
 angle=0
 while(angle<=345):
     sine_of_angle=math.sin(angle)
-    print("sine is: ", sine_of_angle)
+    print("sine is: ", round(sine_of_angle, 4))
     
     cosine_of_angle=math.cos(angle)
-    print("cosine is: ", cosine_of_angle)
+    print("cosine is: ", round(cosine_of_angle, 4))
     angle+=15
     
     
