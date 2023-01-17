e32b3a566ade28a07d351ec5a62e0cc4177f1a83
sp9324 <116882883+sp9324@users.noreply.github.com>
2023-01-17 16:44:27 +0530

    Delete q2.py

--- assignments/q2.py
+++
@@ -1,8 +0,0 @@
-year=int(input("enter year: "))
-if year%4==0:
-    if year%100==0 and year%400==0:
-        print("leap year")
-    elif year%100==0 and year%400!=0:
-        print("not leap year")
-else:
-    print("not leap year")                
